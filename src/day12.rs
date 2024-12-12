8e35a571dd9d032ca6a0bd9ccb27b8d875a9559e
Rufus Uttley <rufusuttley@live.co.uk>
2024-12-12 09:54:02 +0000

    Day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,6 +1,12 @@
 use std::collections::HashSet;
 
 const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+const CORNERS: [((isize, isize), (isize, isize)); 4] = [
+    ((0, -1), (1, 0)),
+    ((1, 0), (0, 1)),
+    ((0, 1), (-1, 0)),
+    ((-1, 0), (0, -1)),
+];
 
 struct Map {
     tiles: Vec<char>,
@@ -50,11 +56,33 @@ impl Map {
             .sum::<usize>()
     }
 
-    fn score_region(
+    fn count_corners(&self, pos: (isize, isize)) -> usize {
+        let mut count = 0;
+
+        let centre = self.get_tile(pos);
+
+        for &(d1, d2) in &CORNERS {
+            let d1_tile = self.get_tile((pos.0 + d1.0, pos.1 + d1.1));
+            let d2_tile = self.get_tile((pos.0 + d2.0, pos.1 + d2.1));
+            let diag_tile = self.get_tile((pos.0 + d1.0 + d2.0, pos.1 + d1.1 + d2.1));
+
+            if d1_tile != centre && d2_tile != centre {
+                count += 1;
+            }
+
+            if d1_tile == centre && d2_tile == centre && diag_tile != centre {
+                count += 1;
+            }
+        }
+
+        count
+    }
+
+    fn explore_area(
         &self,
         start: (isize, isize),
         visited: &mut HashSet<(isize, isize)>,
-    ) -> Option<usize> {
+    ) -> Option<(usize, usize, usize)> {
         // If we've already visited this tile, return
         if !visited.insert(start) {
             return None;
@@ -65,10 +93,12 @@ impl Map {
 
         let mut queue = vec![start];
         let mut perimeter = 0;
+        let mut corners = 0;
         let mut count = 0;
 
         while let Some(pos) = queue.pop() {
             perimeter += self.count_edges(pos);
+            corners += self.count_corners(pos);
             count += 1;
 
             for &(dx, dy) in &DIRECTIONS {
@@ -79,26 +109,29 @@ impl Map {
             }
         }
 
-        Some(perimeter * count)
+        Some((count, perimeter, corners))
     }
 }
 
 pub fn solution(input: &str) {
     let map = Map::new(input).unwrap();
 
-    let mut visited = HashSet::new();
+    let mut part1 = 0;
+    let mut part2 = 0;
 
-    let mut score = 0;
+    let mut visited = HashSet::new();
     for y in 0..map.height {
         for x in 0..map.width {
             let pos = (x as isize, y as isize);
-            if let Some(s) = map.score_region(pos, &mut visited) {
-                score += s;
+            if let Some((c, p, cr)) = map.explore_area(pos, &mut visited) {
+                part1 += c * p;
+                part2 += c * cr;
             }
         }
     }
 
-    println!("Part 1: {}", score);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 #[cfg(test)]
@@ -113,5 +146,25 @@ mod tests {
         assert_eq!(map.count_edges((0, 0)), 2);
         assert_eq!(map.count_edges((1, 0)), 2);
         assert_eq!(map.count_edges((1, 1)), 4);
+    }
+
+    #[test]
+    fn test_count_corners() {
+        let input = "###\n#.#\n###";
+        let map = Map::new(input).unwrap();
+
+        assert_eq!(map.count_corners((0, 0)), 2);
+        assert_eq!(map.count_corners((1, 0)), 0);
+        assert_eq!(map.count_corners((1, 1)), 4);
+    }
+
+    #[test]
+    fn test_explore_area() {
+        let input = "###\n#.#\n###";
+        let map = Map::new(input).unwrap();
+
+        let mut visited = HashSet::new();
+        assert_eq!(map.explore_area((0, 0), &mut visited), Some((8, 16, 8)));
+        assert_eq!(map.explore_area((1, 1), &mut visited), Some((1, 4, 4)));
     }
 }
